ddbc32d854a02b6a1832d01ee074516e212571f4
h-morioka0-gw <h.morioka@gamewith.co.jp>
2023-10-23 10:14:37 +0900

    ジャンプ処理を実装

--- Assets/App/Script/PlayerController.cs
+++ Assets/App/Script/PlayerController.cs
@@ -5,25 +5,44 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // 攻撃判定用オブジェクト.
-    [SerializeField] GameObject attackHit = null;
+    [SerializeField] 
+    private GameObject attackHit = null;
+
+    // 設置判定用ColliderCall.
+    [SerializeField] 
+    ColliderCallReceiver footColliderCall = null;
+
+    // ジャンプ力.
+    [SerializeField] 
+    private float jumpPower = 20f;
+
     // アニメーター.
     Animator animator = null;
-    //! 攻撃アニメーション中フラグ.
+
+    // リジッドボディ.
+    Rigidbody rigid = null;
+
+    // 攻撃アニメーション中フラグ.
     bool isAttack = false;
+
+    // 接地フラグ.
+    bool isGround = false;
 
     // Start is called before the first frame update
     void Start()
     {
         // Animatorを取得し保管.
         animator = GetComponent<Animator>();
+        
+        // Rigidbodyの取得.
+        rigid = GetComponent<Rigidbody>();
+
         // 攻撃判定用オブジェクトを非表示に.
         attackHit.SetActive( false );
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // FootSphereのイベント登録.
+        footColliderCall.TriggerStayEvent.AddListener(OnFootTriggerStay);
+        footColliderCall.TriggerExitEvent.AddListener(OnFootTriggerExit);
     }
 
     // ---------------------------------------------------------------------
@@ -47,7 +66,7 @@ public class PlayerController : MonoBehaviour
     /// 攻撃アニメーションHitイベントコール.
     /// </summary>
     // ---------------------------------------------------------------------
-    void Anim_AttackHit()
+    public void Anim_AttackHit()
     {
         Debug.Log( "Hit" );
         // 攻撃判定用オブジェクトを表示.
@@ -59,12 +78,56 @@ public class PlayerController : MonoBehaviour
     /// 攻撃アニメーション終了イベントコール.
     /// </summary>
     // ---------------------------------------------------------------------
-    void Anim_AttackEnd()
+    public void Anim_AttackEnd()
     {
         Debug.Log( "End" );
         // 攻撃判定用オブジェクトを非表示に.
         attackHit.SetActive( false );
         // 攻撃終了.
         isAttack = false;
+    }
+
+    // ---------------------------------------------------------------------
+    /// <summary>
+    /// ジャンプボタンクリックコールバック.
+    /// </summary>
+    // ---------------------------------------------------------------------
+    public void OnJumpButtonClicked()
+    {
+        if (isGround)
+        {
+            rigid.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+        }
+
+    }
+
+    // ---------------------------------------------------------------------
+    /// <summary>
+    /// FootSphereトリガーステイコール.
+    /// </summary>
+    /// <param name="col"> 侵入したコライダー. </param>
+    // ---------------------------------------------------------------------
+    void OnFootTriggerStay(Collider col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            if (isGround == false) isGround = true;
+            if (animator.GetBool("isGround") == false) animator.SetBool("isGround", true);
+        }
+    }
+
+    // ---------------------------------------------------------------------
+    /// <summary>
+    /// FootSphereトリガーイグジットコール.
+    /// </summary>
+    /// <param name="col"> 侵入したコライダー. </param>
+    // ---------------------------------------------------------------------
+    void OnFootTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "Ground")
+        {
+            isGround = false;
+            animator.SetBool("isGround", false);
+        }
     }
 }
